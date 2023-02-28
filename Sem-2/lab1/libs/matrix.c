583073d4f0d747b7ee4324f529c773ee25c40e24
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-03-01 02:10:54 +0300

    matrix to_string method refactor

--- Sem-2/lab1/libs/matrix.c
+++ Sem-2/lab1/libs/matrix.c
@@ -57,9 +57,9 @@ void matrix_set_value(matrix_t* self, size_t i, size_t j, const void* value) {
 matrix_t* matrix_transpose(matrix_t* self) {
     matrix_t* result = matrix_ctor(type_copy(self->type), matrix_get_width(self), matrix_get_height(self));
     for (size_t j = 0; j < matrix_get_width(self); ++j) {
-        for (size_t i = 0; i < matrix_get_height(self); ++i) {
+        for (size_t i = 0; i < matrix_get_height(self); ++i)
             matrix_set_value(result, j, i, matrix_get_value(self, i, j));
-        }
+
     }
     return result;
 }
@@ -70,7 +70,6 @@ char* matrix_to_string(matrix_t* self) {
     char* buff = (char*)malloc(sizeof(char) * 256);
     if (str == NULL || buff == NULL)
         log_error_and_exit("can't allocate memory", 3);
-    str[0] = '\0';
     size_t str_len = 0;
 
     snprintf(
@@ -85,34 +84,21 @@ char* matrix_to_string(matrix_t* self) {
     str_len += strlen(buff);
 
     for (size_t i = 0; i < matrix_get_height(self); ++i) {
-        if (i == 0) strcat(str + str_len, "[");
-        else strcat(str + str_len, " ");
-        str_len++;
-
-        char* first_value_str = self->type->to_string(matrix_get_value(self, i, 0));
-        snprintf(buff, 256, "%s", first_value_str);
-        free(first_value_str);
-        strncat(str + str_len, buff, strlen(buff));
-        str_len += strlen(buff);
-        for (size_t j = 1; j < matrix_get_width(self); ++j) {
-            strcat(str + str_len, ", ");
-            str_len += 2;
+        for (size_t j = 0; j < matrix_get_width(self); ++j) {
             char* value_str = self->type->to_string(matrix_get_value(self, i, j));
             snprintf(buff, 256, "%s", value_str);
             free(value_str);
             strncat(str + str_len, buff, strlen(buff));
             str_len += strlen(buff);
-        }
 
-        if (i == matrix_get_height(self) - 1) {
-            strcat(str + str_len, "]");
+            strcat(str + str_len, " ");
             str_len++;
         }
         strcat(str + str_len, "\n");
         str_len++;
     }
-
     free(buff);
+
     str = realloc(str, sizeof(char) * strlen(str));
     if (str == NULL)
         log_error_and_exit("can't allocate memory", 3);
