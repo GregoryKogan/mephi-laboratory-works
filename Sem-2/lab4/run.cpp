cc281b2aad60458179972a34195346ee26835911
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-05-21 12:18:07 +0300

    lab4 setup

---
+++ Sem-2/lab4/run.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Gregory Kogan on 21.05.2023.
+//
+
+#include <sequence_lib.hpp>
+
+
+int main() {
+    sequence_lib_is_working();
+    return 0;
+}
---
+++ Sem-2/lab4/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gregory Kogan on 21.05.2023.
+//
+
+#include <test_lib.hpp>
+
+
+int main() {
+    test_lib_is_working();
+    return 0;
+}
+
