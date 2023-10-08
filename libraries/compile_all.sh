00a5e7ecb72c14c571074b058321122fa6f7b30a
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-08 19:10:15 +0300

    lib dir bug fixed

--- libraries/compile_all.sh
+++ libraries/compile_all.sh
@@ -7,18 +7,21 @@ libraries_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$libraries_path"
 
 # test_lib
+rm -rf test_lib/lib; mkdir test_lib/lib
 cd test_lib/source
 cmake . && make
 mv libtest_lib.a ../lib
 cd "$libraries_path"
 
 # sequence_lib
+rm -rf sequence_lib/lib; mkdir sequence_lib/lib
 cd sequence_lib/source
 cmake . && make
 mv libsequence_lib.a ../lib
 cd "$libraries_path"
 
 # container_lib
+rm -rf container_lib/lib; mkdir container_lib/lib
 cd container_lib/source
 cmake . && make
 mv libcontainer_lib.a ../lib
