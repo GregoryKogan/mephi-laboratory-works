fa3e7ab0bf5f65765fd5ed7c93070148b28779ee
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-18 22:14:43 +0300

    frontend initialized

---
+++ Sem-3/lab1/interface/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1>Hello, world!</h1>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
