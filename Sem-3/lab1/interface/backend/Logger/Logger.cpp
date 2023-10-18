b4d0aadc025893a23616fbc6dc5ac589ac6031dc
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-18 23:55:13 +0300

    timestamp added

--- Sem-3/lab1/interface/backend/Logger/Logger.cpp
+++ Sem-3/lab1/interface/backend/Logger/Logger.cpp
@@ -1,5 +1,13 @@
 #include "Logger.hpp"
 
-void kogan::Logger::Logger::log(const httplib::Request& req, const httplib::Response& res) {
-    log_stream << req.method << " " << req.path << " " << res.status << std::endl;
+void kogan::Logger::Logger::log(const httplib::Request& req, const httplib::Response& res) const noexcept {
+    log_stream << get_timestamp() << " " << req.method << " " << req.path << " " << res.status << std::endl;
+}
+
+std::string kogan::Logger::Logger::get_timestamp() const noexcept {
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    char buf[100] = {0};
+    std::strftime(buf, sizeof(buf), "%Y-%m-%d.%X", std::localtime(&now));
+    return buf;
 }
